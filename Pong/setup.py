fecee9feed509269a888a93b36b944b244141934
Dumbass Noob <core.contact@yahoo.com>
2026-02-23 13:51:14 +0100

    Auto install for fedora, arch and suse.

--- Pong/setup.py
+++ Pong/setup.py
@@ -1,7 +1,21 @@
 from os import system as cmd
 
+def fedoraInstall() {
+print('Attempting to install raylib for fedora...')
+cmd('sudo dnf install raylib raylib-devel')
+}
+def archInstall() {
+print('Attempting to install raylib for Arch...')
+cmd('yay -S raylib')
+}
+def suseInstall() {
+print("Attempting to install raylib for openSUSE...')
+cmd('zypper in raylib-devel')
+}
+
+
 a = input("Is raylib installed? (Y/n) ")
-if a != "y" or a != "":
+if a != "n":
     print("""Please install raylib.
             Fedora:
             sudo dnf install raylib raylib-devel
@@ -12,6 +26,15 @@ if a != "y" or a != "":
 
         Other distros:
         https://github.com/raysan5/raylib/wiki/Working-on-GNU-Linux""")
+        b = ('Want to install? (no,fedora,suse,arch): ')
+        if b == 'fedora':
+            fedoraInstall()
+        elif b == 'arch':
+            archInstall()
+        elif b == 'suse':
+            suseInstall()
+        else:
+            print("You either declined or your input wasn't correctly handled. Make sure that you didn't add a space or uppercase letter.")
 else:
     print("Attempting to build the game...")
     try:
