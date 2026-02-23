41b52436880e1f6dc78cf4f7812fae144bc8e837
Dumbass Noob <core.contact@yahoo.com>
2026-02-23 13:35:38 +0100

    Rewrote variables to make them more human readable and added ball radius to config.
    - snake_case for variables
    - Added more english descriptions

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -7,14 +7,27 @@
 
 
         int screen_width = 800; // Zmiana szerokosci okna
+								// Window width
         int screen_height = 500; // Zmiana wysokosci okna
-	int scorel = 0; // Poczatkowy wynik gracza lewego
-	int scorer = 0; // ------------==--------- prawego
+								 // Window height
+	int score_left = 0; // Poczatkowy wynik gracza lewego
+						// Beginning score of left player
+	int score_right = 0; // ------------==--------- prawego
+						 // Same as before but for right player
 	int MaxFps = 45; // Maksymalna liczba FPS
-	int speedBall = 7; // Predkosc pileczki (ostrożnie)
-	int paddlSpeed = 5; // Predkosc paletek do ping ponga
-	std::string mainCol = "RED";     // Zmiana koloru glownego (zalecany: "default main")
-	std::string secCol = "BLUE"; // Zmiana koloru pobocznego (zalecany: "default secondary")
+					 // Max FPS limit
+	int speed_ball = 7; // Predkosc pileczki (ostrożnie)
+						// Ball speed (careful)
+    int ball_radius     // Promień koła (piłki)
+                        // Radius of the circle (ball)
+	int paddle_speed = 5; // Predkosc paletek do ping ponga
+						// Speed of ping pong paddles
+	std::string mainColor = "RED";     // Zmiana koloru glownego (zalecany: "default main")
+									   // Changing main color (recommended: "default main")
+	std::string secColor = "BLUE"; // Zmiana koloru pobocznego (zalecany: "default secondary")
+									// Changing secondary color (recommended: "default secondary")
+						  // |------ Table of colors -----------|
+						  // |name:                             |
 						  // |------ Tabela Kolorow ------------|
 						  // |nazwa:                Tlumaczenie:|
 						  // |----------------------------------|
@@ -27,7 +40,7 @@
 						  //  PURPLE       -        Fioletowy
 						  //  ORANGE       -        Pomaranczowy
 						  //  GRAY         -        Szary
-
+						  //  This part is self axplainatory in english.
 
 //------------------------------------------------------------------------------------------------------------------------------//
 
@@ -66,22 +79,21 @@ class Ball{
     public:
     float x, y;
     int speed_x, speed_y;
-    int radius;
     int speed;
 
     void draw() {
-        DrawCircle(x, y, radius, getCol(secCol));
+        DrawCircle(x, y, ball_radius, getCol(secColor));
     }
 
     void update() {
         x += speed_x;
         y += speed_y;
 
-        if(y + radius >= GetScreenHeight() || y - radius <= 0)
+        if(y + ball_radius >= GetScreenHeight() || y - radius <= 0)
         {
             speed_y *= -1;
         }
-        if(x + radius >= GetScreenWidth() || x - radius <= 0)
+        if(x + ball_radius >= GetScreenWidth() || x - radius <= 0)
         {
             speed_x *= -1;
         }
@@ -96,7 +108,7 @@ class PaddleArrow {
 
         void draw()
         {
-            DrawRectangle(x, y, width, height, getCol(secCol));
+            DrawRectangle(x, y, width, height, getCol(secColor));
         }
                     void prevent_leaving()
                     {
@@ -150,23 +162,22 @@ int main()
     InitWindow(screen_width, screen_height, "Game");
     SetTargetFPS(MaxFps);
     
-    ball.radius = 2;
     ball.x = screen_width/2;
     ball.y = screen_height/2;
-    ball.speed_x = speedBall;
-    ball.speed_y = speedBall;
+    ball.speed_x = ball_speed;
+    ball.speed_y = ball_speed;
 
     paddlel.height = 150;
     paddlel.width = 30;
     paddlel.x = 0;
     paddlel.y = screen_height/2 - paddlel.height/2;
-    paddlel.speed = paddlSpeed;
+    paddlel.speed = paddle_speed;
 
     paddler.height = 150;
     paddler.width = 30;
     paddler.x = screen_width - paddler.width;
     paddler.y = screen_height/2 - paddler.height/2;
-    paddler.speed = paddlSpeed;
+    paddler.speed = paddle_speed;
 
     while(WindowShouldClose() == false)
     {
@@ -197,15 +208,15 @@ int main()
         paddler.prevent_leaving();
         
     
-        ClearBackground(getCol(mainCol));
-        DrawLine(screen_width/2, 0, screen_width/2, screen_height, getCol(secCol));
+        ClearBackground(getCol(mainColor));
+        DrawLine(screen_width/2, 0, screen_width/2, screen_height, getCol(secColor));
         ball.draw();
 
         paddlel.draw();
         paddler.draw();
 
-	DrawText(TextFormat("Score: %d", scorel), GetScreenWidth()/4-GetScreenHeight()/20, GetScreenHeight()/10, GetScreenHeight()/20, getCol(secCol)); //lewa paletka
-	DrawText(TextFormat("Score: %d", scorer), GetScreenWidth()/4*3-GetScreenHeight()/20, GetScreenHeight()/10, GetScreenHeight()/20, getCol(secCol)); //prawa paletka
+	DrawText(TextFormat("Score: %d", score_left), GetScreenWidth()/4-GetScreenHeight()/20, GetScreenHeight()/10, GetScreenHeight()/20, getCol(secColor)); //lewa paletka
+	DrawText(TextFormat("Score: %d", score_right), GetScreenWidth()/4*3-GetScreenHeight()/20, GetScreenHeight()/10, GetScreenHeight()/20, getCol(secColor)); //prawa paletka
         EndDrawing();
     }
 
